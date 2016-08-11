1d668b64745a7b876ea5dd2d39834f20e8dd74ec
Ahmed Shaaban & George Green & Madeleine <pair+AhmedShab.george-fg.madeleine-rashbrooke@devbootcamp.com>
2016-08-12 11:54:47 +1200

    Initial set up of player class objects, some scenarios

---
+++ content.js
@@ -0,0 +1,25 @@
+//this is just text at the moment
+id: 1
+name: "accountability-group"
+time: "08:45"
+text: "The question of the day is 'What kind of monkey is Joseph
+and how do monkeys demonstrate EDA's values of Integrity, Kindness and Effort?'
+The group embarks on an philosophical discussion of whether non-humans can be kind
+rather than just nice."
+goodDecisionText: "You arrive on time and share valuable insights about monkeys (or about Joseph)"
+badDecisionText: "You sleep in, arrive late, make a coffee and have a rant about Donald Trump"
+
+id: 2
+name: "friday-feels"
+time: "09:00"
+text: "The bell rings and you join your Friday Feels group for two minutes of
+sharing your feelings about the week and your Learning."
+goodDecisionText: ""
+badDecisionText: ""
+
+id:
+name: ""
+time: ""
+text: ""
+goodDecisionText: ""
+badDecisionText: ""
