dbec10aaeaf7bf49f35196dd443706b099e99527
Ahmed Shaaban & George Green & Madeleine <pair+AhmedShab.george-fg.madeleine-rashbrooke@devbootcamp.com>
2016-08-12 13:19:41 +1200

    Last changes before branching

--- content.js
+++ content.js
@@ -1,24 +1,40 @@
 //this is just text at the moment
 id: 1
-name: "accountability-group"
+"name": "accountability-group"
 time: "08:45"
 text: "The question of the day is 'What kind of monkey is Joseph
 and how do monkeys demonstrate EDA's values of Integrity, Kindness and Effort?'
 The group embarks on an philosophical discussion of whether non-humans can be kind
 rather than just nice."
-goodDecisionText: "You arrive on time and share valuable insights about monkeys (or about Joseph)"
+goodDecisionText: "You arrive on time and share valuable insights about monkeys (or about Joseph, your choice)"
 badDecisionText: "You sleep in, arrive late, make a coffee and have a rant about Donald Trump"
 
 id: 2
-name: "friday-feels"
+"name": "friday-feels"
 time: "09:00"
 text: "The bell rings and you join your Friday Feels group for two minutes of
 sharing your feelings about the week and your Learning."
+goodDecisionText: "Taking exactly two minutes, you coherently describe the key
+points of your Learning and develop new insights in the Meaning of Life."
+badDecisionText: "You spend your two minutes talking about the People of EDA wall.
+Rohan very Kindly suggests you've missed the point of the exercise."
+
+id:
+"name": ""
+time: ""
+text: ""
 goodDecisionText: ""
 badDecisionText: ""
 
 id:
-name: ""
+"name": ""
+time: ""
+text: ""
+goodDecisionText: ""
+badDecisionText: ""
+
+id:
+"name": ""
 time: ""
 text: ""
 goodDecisionText: ""
